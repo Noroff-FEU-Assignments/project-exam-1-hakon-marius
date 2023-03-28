f63260a35db3b3e88a36669ac41eae6f5b278bef
hakon-marius <hakon.marius.solvang@gmail.com>
2023-03-29 00:46:35 +0200

    mvd

--- js/blog.js
+++ js/blog.js
@@ -5,26 +5,25 @@ const blogContainer = document.querySelector("#blogContainer");
 
 async function getBlogData() {
 
-    let url = 'https://eboe.no/eboe/wp-json/wp/v2/posts';
-
+    let url = 'https://eboe.no/eboe/wp-json/wp/v2/posts/?per_page=10';
 
     try {
 
         const response = await fetch(url);
         const posts = await response.json();
 
+
+        console.log(posts)
         posts.forEach(post => {
 
 
 
             postTitle = post.title.rendered;
             postContent = post.content.rendered;
-
-
-            blogContainer.innerHTML += `<div class="singlepost"><a href="#"><div><h2>${postTitle}</h2>${postContent}</div> </div></a>`
-
-
+            blogContainer.innerHTML += `<div class="singlepost" id="${post.id}"><div><h2 class="postTitle">${postTitle}</h2>${postContent}<a href="#"class="linkToFullPost">Read more
+            </a></div></div>`
         });
+
 
 
     } catch (error) {
@@ -33,6 +32,4 @@ async function getBlogData() {
 
 }
 
-
 getBlogData()
-
